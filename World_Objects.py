67c9fa2dccd13b1177918e9433d0eee70f901bd3
Corey Verkouteren <waffleme@mail.com>
2023-03-22 09:57:57 -0400

    Pixel Crate
    Made the power up crate pixelated, needs more changes tho I think

--- World_Objects.py
+++ World_Objects.py
@@ -15,7 +15,7 @@ class PowerUp(arc.Sprite):
 class PowerUpBox(arc.Sprite):
     def __init__(self):
         super().__init__()
-        self.texture = arc.load_texture(":resources:images/tiles/boxCrate_single.png")
+        self.texture = arc.load_texture("Assets/Powerups/power_up_box.png")
         self.scale = .3
 
 
